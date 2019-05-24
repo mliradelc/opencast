752975b06a6809324f56ab211eb94e07a4b40449
Maximiliano Lira Del Canto <8040628+mliradelc@users.noreply.github.com>
2019-05-24 21:05:36 +0200

    Theodul configuration settings in Ui-Config

--- modules/engage-theodul-core/src/main/resources/ui/js/engage/models/meInfo.js
+++ modules/engage-theodul-core/src/main/resources/ui/js/engage/models/meInfo.js
@@ -20,10 +20,11 @@
  */
 /*jslint browser: true, nomen: true*/
 /*global define, CustomEvent*/
-define(['jquery', 'backbone'], function($, Backbone) {
+define(['jquery', 'backbone', 'js-yaml.min'], function($, Backbone, jsyaml) {
     "use strict";
 
-    var prop_shortcut = "player.shortcut.",
+    var configURL = "/ui/config/theodul/config.yml",
+        prop_shortcut = "player.shortcut.",
         prop_shortcut_sequence = "player.shortcut-sequence",
         prop_allowedtags = "player.allowedtags",
         prop_allowedformats = "player.allowedformats",
@@ -42,8 +43,8 @@ define(['jquery', 'backbone'], function($, Backbone) {
         prop_matomo_track_events = "player.matomo.track_events",
         prop_hide_video_context_menu = "player.hide_video_context_menu",
         ready = false,
-        positioncontrols = "";
-
+        positioncontrols = "",
+        config = "";
     /*
      * Model with information about the current user and the current MH configuration
      */
@@ -69,113 +70,47 @@ define(['jquery', 'backbone'], function($, Backbone) {
                         matomo_heartbeat,
                         matomo_notification,
                         matomo_track_events;
-                    if (me && me.attributes && me.attributes.org && me.attributes.org.properties) {
-                        // extract shortcuts
-                        $.each(me.attributes.org.properties, function(key, value) {
-                            var name = key.substring(prop_shortcut.length, key.length);
-                            // shortcuts
-                            if ((key.indexOf(prop_shortcut) != -1) && (name.length > 0) && value) {
-                                shortcuts.push({
-                                    name: key.substring(prop_shortcut.length, key.length),
-                                    key: value
-                                });
-                            }
-                            // the seuence in which shortcuts should be presented
-                            else if ((key == prop_shortcut_sequence) && value) {
-                                shortcut_sequence = value;
-                            }
-                            // allowed tags on videos that should be played
-                            else if ((key == prop_allowedtags) && value) {
-                                allowedTags = value;
-                            }
-                            // formats that should be played
-                            else if ((key == prop_allowedformats) && value) {
-                                allowedFormats = value;
-                            }
-                            // master video type
-                            else if ((key == prop_mastervideotype) && value) {
-                                mastervideotype = value;
-                            }
-                            // controls position
-                            else if ((key == prop_positioncontrols) && value) {
-                                positioncontrols = value;
-                            }
-                            // controls position
-                            else if ((key == prop_layout) && value) {
-                                layout = value;
-                            }
-                            // controls position
-                            else if ((key == prop_focusedflavor) && value) {
-                                focusedflavor = value;
-                            }
-                            // player logo
-                            else if ((key == prop_logo_mediamodule) && value) {
-                                logo_mediamodule = value;
-                            }
-                            // small logo
-                            else if ((key == prop_logo_player) && value) {
-                                logo_player = value;
-                            }
-                            // link to Media Modul
-                            else if ((key == prop_link_mediamodule) && value) {
-                                if (value.trim() == "true") link_mediamodule = true;
-                            }
-                            // show embed links
-                            else if ((key == prop_show_embed_link) && value) {
-                                if (value.trim() == "true") show_embed_link = true;
-                            }
-                            // hide video context menu
-                            else if ((key == prop_hide_video_context_menu) && value) {
-                              if (value.trim() == "true") hide_video_context_menu = true;
-                            }
-                            // Piwik-Settings
-                            else if ((key == prop_matomo_server) && value) {
-                              matomo_server = value;
-                            }
-                            else if ((key == prop_matomo_site_id) && value) {
-                              matomo_site_id = value;
-                            }
-                            else if ((key == prop_matomo_heartbeat) && value) {
-                              matomo_heartbeat = value;
-			    }
-                            else if ((key == prop_matomo_notification)) {
-                              if (value) {
-                              matomo_notification = value;
-                              } else matomo_notification = true; 
-                            }
-                            else if ((key == prop_matomo_track_events) && value) {
-                              matomo_track_events = value;
-                            }
-                        });
-                    }
-                    me.set("allowedtags", allowedTags);
-                    me.set("allowedformats", allowedFormats);
-                    me.set("shortcuts", shortcuts);
-                    me.set("mastervideotype", mastervideotype);
-                    me.set("logo_mediamodule", logo_mediamodule);
-                    me.set("logo_player", logo_player);
-                    me.set("link_mediamodule", link_mediamodule);
-                    me.set("show_embed_links", show_embed_link);
-                    me.set("hide_video_context_menu", hide_video_context_menu);
-                    me.set("shortcut-sequence", shortcut_sequence);
-                    me.set("layout", layout);
-                    me.set("focusedflavor", focusedflavor);
-                    me.set("matomo.server", matomo_server);
-                    me.set("matomo.site_id", matomo_site_id);
-                    me.set("matomo.heartbeat", matomo_heartbeat);
-                    me.set("matomo.notification", matomo_notification);
-                    me.set("matomo.track_events", matomo_track_events);
-                    ready = true;
-                }
-            });
-        },
-        ready: function() {
-            return ready;
-        },
-        getPositionControls: function() {
-            return positioncontrols;
-        }
-    });
 
-    return MeInfoModel;
+                      $.ajax({
+                        url: configURL,
+                        dataType: "text",
+                        success: function(data) {
+                          var rawfile = data;
+                          }
+                        }).then(function(rawfile){
+                      config = jsyaml.load(rawfile);
+                      me.set("allowedtags", config.allowedTags);
+                      me.set("allowedformats", config.allowedFormats);
+                      me.set("shortcuts", config.shortcuts);
+                      me.set("mastervideotype", config.mastervideotype);
+                      me.set("logo_mediamodule", config.logo_mediamodule);
+                      me.set("logo_player", config.logo_player);
+                      me.set("link_mediamodule", config.link_mediamodule);
+                      me.set("show_embed_links", config.show_embed_link);
+                      me.set("hide_video_context_menu", config.hide_video_context_menu);
+                      me.set("shortcut-sequence", config.shortcut_sequence);
+                      me.set("layout", config.layout);
+                      me.set("focusedflavor", config.focusedflavor);
+                      me.set("matomo.server", config.matomo_server);
+                      me.set("matomo.site_id", config.matomo_site_id);
+                      me.set("matomo.heartbeat", config.matomo_heartbeat);
+                      me.set("matomo.notification", config.matomo_notification);
+                      me.set("matomo.track_events", config.matomo_track_events);
+                      ready = true;
+                      })
+
+                  }
+              });
+          },
+      ready: function() {
+        return ready;
+        },
+      getPositionControls: function() {
+        positioncontrols = config.positioncontrols;
+        return positioncontrols;
+        }
+      });
+      
+
+  return MeInfoModel;
 });
